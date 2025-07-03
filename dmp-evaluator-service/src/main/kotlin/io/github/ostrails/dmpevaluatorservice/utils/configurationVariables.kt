322a8066f42eb455c74248af25900c0dbccad5e4
Andres Tabima <andres.romero@tuwien.ac.at>
2025-07-03 11:50:04 +0200

    Add: Environment variable for the fields in the response of the services

--- dmp-evaluator-service/src/main/kotlin/io/github/ostrails/dmpevaluatorservice/utils/configurationVariables.kt
+++ dmp-evaluator-service/src/main/kotlin/io/github/ostrails/dmpevaluatorservice/utils/configurationVariables.kt
@@ -1,14 +1,31 @@
 package io.github.ostrails.dmpevaluatorservice.utils
 
-import com.mysql.cj.LicenseConfiguration
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.stereotype.Component
+
+@Component
+@ConfigurationProperties(prefix = "dmp.global")
+class ConfigurationGlobalVariables(
+    var repository: String = "",
+    var mitLicense: String = ""
+) {}
 
 @Component
 @ConfigurationProperties(prefix = "dmp.test")
 class ConfigurationTestVariables(
     var endpointURL: String = "",
-    var repository: String = "",
-    var mitLicense: String = "",
 ) {}
+
+@Component
+@ConfigurationProperties(prefix = "dmp.metric")
+class ConfigurationMetricVariables(
+    var endpointURL: String = "",
+) {}
+
+@Component
+@ConfigurationProperties(prefix = "dmp.benchmark")
+class ConfigurationBenchmarkVariables(
+    var endpointURL: String = "",
+) {}
+
 
